0200a741e549e1d3df6f76aa692847206dee98f9
honmapietra <54584172+honmapietra@users.noreply.github.com>
2020-07-30 14:08:58 -0300

    Updates

---
+++ precache-manifest.a7ceb6ae96793247d782ed9d2fa023e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9648e51e8747bc52ee1ec381a35b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af7668312e1e2527695",
+    "url": "/static/css/main.013b23aa.chunk.css"
+  },
+  {
+    "revision": "1bb70883f116379d41d7",
+    "url": "/static/js/2.6704428d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6704428d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af7668312e1e2527695",
+    "url": "/static/js/main.e9c05d5b.chunk.js"
+  },
+  {
+    "revision": "1cd9a3cd802d36721b69",
+    "url": "/static/js/runtime-main.41d2f7f8.js"
+  },
+  {
+    "revision": "18f8ad789968c293388c734f9b689457",
+    "url": "/static/media/Logo.18f8ad78.png"
+  }
+]);
